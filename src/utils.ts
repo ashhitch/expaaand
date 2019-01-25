badb41b9d05acb8c3c9b16e9b99b6bb49c3787e0
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2019-01-25 14:43:11 +0000

    # This is a combination of 8 commits. # This is the 1st commit message:
    first pass
    
    # This is the commit message #2:
    
    linting
    
    # This is the commit message #3:
    
    more linting
    
    # This is the commit message #4:
    
    Bug fixes and example code
    
    # This is the commit message #5:
    
    Default options
    
    # This is the commit message #6:
    
    Typo
    
    # This is the commit message #7:
    
    progress on resize check
    
    # This is the commit message #8:
    
    Update readme and examples

---
+++ src/utils.ts
@@ -0,0 +1,20 @@
+/**
+ * Checks if class exists on element
+ *
+ * @param {HTMLElement} elm
+ * @param {string} className
+ * @returns {boolean}
+ */
+export const hasClass = (elm: HTMLElement, className: string): boolean => {
+  if (typeof className !== 'string') {
+    throw new TypeError('Expected a string as second argument');
+  }
+
+  // New browsers
+  if (elm.classList) {
+    return elm.classList.contains(className);
+  }
+
+  // Older Browsers
+  return new RegExp('(^| )' + className + '( |$)', 'gi').test(elm.className);
+};
